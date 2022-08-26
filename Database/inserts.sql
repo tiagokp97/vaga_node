e86cf6044cf7fdb1817f034bb9432dbe69c52d5c
tiagokp97 <tiagopesch@gmail.com>
2022-08-26 17:06:48 -0300

    build: database inserts

---
+++ Database/inserts.sql
@@ -0,0 +1,42 @@
+INSERT INTO
+  jogador (nome, idade)
+VALUES
+  ('Nadal', 36);
+
+INSERT INTO
+  jogador (nome, idade)
+VALUES
+  ('Federer', 41);
+
+INSERT INTO
+  jogador (nome, idade)
+VALUES
+  ('Murray', 35);
+  
+  INSERT INTO partidas
+(jogador1_id, jogador2_id, pontos_jogador1, pontos_jogador2, duracao)
+VALUES (1, 2, 15, 40, 120);
+
+INSERT INTO partidas
+(jogador1_id, jogador2_id, pontos_jogador1, pontos_jogador2, duracao)
+VALUES (2, 3, 30, 15, 91);
+
+INSERT INTO partidas
+(jogador1_id, jogador2_id, pontos_jogador1, pontos_jogador2, duracao)
+VALUES (2, 3, 30, 15, 91);
+
+INSERT INTO partidas
+(jogador1_id, jogador2_id, pontos_jogador1, pontos_jogador2, duracao)
+VALUES (2, 3, 30, 15, 91);
+
+INSERT INTO partidas
+(jogador1_id, jogador2_id, pontos_jogador1, pontos_jogador2, duracao)
+VALUES (3, 1, 30, 30, 95);
+
+INSERT INTO partidas
+(jogador1_id, jogador2_id, pontos_jogador1, pontos_jogador2, duracao)
+VALUES (1, 2, 15, 15, 100);
+
+INSERT INTO partidas
+(jogador1_id, jogador2_id, pontos_jogador1, pontos_jogador2, duracao)
+VALUES (1, 2, 15, 40, 100);
